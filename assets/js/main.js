776bcbeb55e821a50c875299d75db8982ea0df46
PM <piermario.salis@aol.com>
2020-12-02 19:49:33 +0100

    Init
    Definisco funzione che crea numero randomico;
    Definisco variabili per un numero variabile generato dal computer;
    Definisco variabile per contenere i numeri che inserirà l'utente.

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// Definisco variabili per i numeri random e i numeri inseriti dall'utente
+var randomNumber = [];
+
+var userNumber = [];
+
+
+while (randomNumber < 5) {
+  var number = createRandom(1,100);
+  randomNumber.push(number);
+  
+
+}
+
+// definisco una funzione che crea numeri randomici compresi fra l'uno e il cento
+
+function createRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
